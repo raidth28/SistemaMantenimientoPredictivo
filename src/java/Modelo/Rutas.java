53bccbefd47995f224f31dc939549c9a014e2a3d
raidth28 <raidthv@gmail.com>
2025-10-15 13:30:31 -0500

    KRISTHOR 2
    ...

--- src/java/Modelo/Rutas.java
+++ src/java/Modelo/Rutas.java
@@ -9,5 +9,38 @@ package Modelo;
  * @author Raidth
  */
 public class Rutas {
+    private int idRuta;
+    private String nombreRuta;
+    private String descripcion;
+
+    public Rutas(int idRuta, String nombreRuta, String descripcion) {
+        this.idRuta = idRuta;
+        this.nombreRuta = nombreRuta;
+        this.descripcion = descripcion;
+    }
+
+    public int getIdRuta() {
+        return idRuta;
+    }
+
+    public void setIdRuta(int idRuta) {
+        this.idRuta = idRuta;
+    }
+
+    public String getNombreRuta() {
+        return nombreRuta;
+    }
+
+    public void setNombreRuta(String nombreRuta) {
+        this.nombreRuta = nombreRuta;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
     
 }
--- src/java/Modelo/TipoAlertas.java
+++ src/java/Modelo/TipoAlertas.java
@@ -9,5 +9,38 @@ package Modelo;
  * @author Raidth
  */
 public class TipoAlertas {
+    private int idTipoAlerta;
+    private String nombreTipo;
+    private String prioridad;
+
+    public TipoAlertas(int idTipoAlerta, String nombreTipo, String prioridad) {
+        this.idTipoAlerta = idTipoAlerta;
+        this.nombreTipo = nombreTipo;
+        this.prioridad = prioridad;
+    }
+
+    public int getIdTipoAlerta() {
+        return idTipoAlerta;
+    }
+
+    public void setIdTipoAlerta(int idTipoAlerta) {
+        this.idTipoAlerta = idTipoAlerta;
+    }
+
+    public String getNombreTipo() {
+        return nombreTipo;
+    }
+
+    public void setNombreTipo(String nombreTipo) {
+        this.nombreTipo = nombreTipo;
+    }
+
+    public String getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
     
 }
